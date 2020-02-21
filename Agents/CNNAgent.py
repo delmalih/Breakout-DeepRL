5d78104759f633cf34d3ae47abdb4dc203466d2d
dave1496 <dav_elmaleh@hotmail.com>
2020-02-21 18:28:29 +0100

    debut: Prints

--- Agents/CNNAgent.py
+++ Agents/CNNAgent.py
@@ -85,7 +85,7 @@ class CNNAgent(BaselineAgent):
         score = 0
         for e in range(n_epochs):
             state = self.__env.reset()
-            lives = 5
+            n_lives = 5
             done = False
             while not done:
                 action = self.act(state)
@@ -93,12 +93,12 @@ class CNNAgent(BaselineAgent):
                 score += reward
                 loss = self.reinforce(state, next_state, action, reward, done)
                 state = next_state
-                print(info)
-                if lives != info["ale.lives"]:
-                    lives = info["ale.lives"]
+                print(info["ale.lives"])
+                if n_lives != info["ale.lives"]:
+                    n_lives = info["ale.lives"]
 
                 print("Epoch {:03d}/{:03d} - {} lives | Loss {:.4f} | Score {}"
-                      .format(e, lives, n_epochs, loss, score))
+                      .format(e, n_lives, n_epochs, loss, score))
 
             self.__env.draw_video(output_path + "/" + str(e))
             self.save(name_weights=output_path + 'model.h5',

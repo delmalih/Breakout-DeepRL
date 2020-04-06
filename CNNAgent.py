970f3bdb45e87f9d51a868fe47e9f7e0c862e7b0
dave1496 <dav_elmaleh@hotmail.com>
2020-04-06 17:26:21 +0200

    feat: update grad

--- CNNAgent.py
+++ CNNAgent.py
@@ -67,12 +67,12 @@ class CNNAgent(BaselineAgent):
 
     def reinforce(self, state, next_state, action, reward, done):
         self.optimizer.zero_grad()
-        input_q_values = self.model(state)
-        next_q_values = self.model(next_state)
+        with torch.no_grad():
+            next_q_values = self.model(next_state)
         target_q = torch.zeros(input_q_values.shape).to(self.device)
         target_q[done == 1, action[done == 1]] = reward[done == 1]
         target_q[done == 0, action[done == 0]] = (reward + torch.max(next_q_values, dim=-1)[0])[done == 0]
-        loss = torch.mean(torch.pow(input_q_values - target_q, 2))
+        loss = torch.mean(torch.pow(self.model(state) - target_q, 2))
         loss.backward()
         self.optimizer.step()
         return loss.item()

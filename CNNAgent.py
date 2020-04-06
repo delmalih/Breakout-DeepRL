7102918f55b5980132ea5624bb659d72a8e5b9fb
dave1496 <dav_elmaleh@hotmail.com>
2020-04-06 16:32:39 +0200

    fix

--- CNNAgent.py
+++ CNNAgent.py
@@ -74,6 +74,7 @@ class CNNAgent(BaselineAgent):
         next_q_values = self.model(next_state)
         target_q = torch.zeros(input_q_values.shape).to(self.device)
         target_q[done == 1, action[done == 1]] = reward[done == 1]
+        print(torch.max(next_q_values, dim=-1))
         target_q[done == 0, action[done == 0]] = (reward + torch.max(next_q_values, dim=-1).values)[done == 0]
         loss = torch.mean(torch.pow(input_q_values - target_q, 2))
         loss.backward()

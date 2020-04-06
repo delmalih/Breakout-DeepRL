eb670044973dc9f25c34e03703d58cbd803b87f6
dave1496 <dav_elmaleh@hotmail.com>
2020-04-06 22:11:43 +0200

    feat: edit constants

--- CNNModel.py
+++ CNNModel.py
@@ -21,34 +21,18 @@ class CNNModel(nn.Module):
         self.conv_layers = nn.Sequential(
             # <-- Block 1 --> #
             nn.Conv2d(constants.N_CHANNELS, 16, 3, padding=1),
-            nn.BatchNorm2d(16),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(16, 16, 3, padding=1),
-            nn.BatchNorm2d(16),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             # <-- Block 2 --> #
             nn.Conv2d(16, 32, 3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(32, 32, 3, padding=1),
-            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             # <-- Block 3 --> #
             nn.Conv2d(32, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             # <-- Block 4 --> #
             nn.Conv2d(64, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
         )
